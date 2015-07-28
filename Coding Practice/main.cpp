b557056ca33ee9b0620f9eb53b7b1a5495394fc8
Kulsoom Mansoor <kulsoommansoor1@Kulsooms-Macintosh.local>
2015-07-27 22:31:51 -0700

    Initial Commit

---
+++ Coding Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coding Practice
+//
+//  Created by Kulsoom Mansoor on 7/27/15.
+//  Copyright (c) 2015 Kulsoom Mansoor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
